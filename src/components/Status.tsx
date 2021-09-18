766dfde15d029b586a0bdb286295199e09017c94
moebiusmania <moebiusmania@gmail.com>
2021-09-18 12:13:31 +0200

    feat: adding custom colors, loading state in status, polishing status card

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -8,21 +8,38 @@ type LineStatus = {
   status: string,
 }
 
+const mock: LineStatus = {
+  line: 'foo',
+  text: 'bar',
+  status: 'pippo'
+}
+
+const mockData: LineStatus[] = Array(10).fill(mock);
+
 const Status = (): JSX.Element => {
-  const [data, setData] = useState<LineStatus[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<LineStatus[]>(mockData);
   const resource: string = "https://atmmi-info-api.herokuapp.com/status";
+
+  const loadingClass = (): string => {
+    const value: number = Math.ceil(Math.random() * 4);
+    return `h-4 bg-gray-400 rounded w-${value}/4`;
+  }
 
   useEffect(() => {
     fetch(resource)
       .then((response: Response): Promise<any> => response.json())
-      .then((json: LineStatus[]) => setData(json));
+      .then((json: LineStatus[]) => setData(json))
+      .then(() => setLoading(false));
   }, []);
 
   return (
-    <Card title="Status">
+    <Card title="Status linee MM">
       <ul>
         {data.map((e: LineStatus, i: number): JSX.Element => 
-          <li key={i}>{e.line} | {e.text} | {e.status} </li>)}
+         loading ?
+          <li key={i} className="my-2 animate-pulse"><div className={loadingClass()}></div></li> :
+          <li key={i}><div className={`badge bg-line-${e.line} rounded-none border-0`}>{e.line}</div> {e.text} | {e.status} </li> )}
       </ul>
     </Card>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f48325',
+        'line-M1': '#E31D1A',
+        'line-M2': '#638B18',
+        'line-M3': '#F6A704',
+        'line-M5': '#9778D3',
+      }
+    },
   },
   variants: {
     extend: {},
