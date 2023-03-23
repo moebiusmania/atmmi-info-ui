1f58a0e65fcbafca75df7b3187fa2ed03dc67cef
moebiusmania <moebiusmania@gmail.com>
2023-03-23 21:31:14 +0100

    feat: add M4 color variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
   ],
-  safelist: ["bg-line-M1", "bg-line-M2", "bg-line-M3", "bg-line-M5"],
+  safelist: ["bg-line-M1", "bg-line-M2", "bg-line-M3", "bg-line-M4", "bg-line-M5"],
   theme: {
     extend: {
       colors: {
@@ -10,6 +10,7 @@ module.exports = {
         "line-M1": "#E31D1A",
         "line-M2": "#638B18",
         "line-M3": "#F6A704",
+        "line-M4": "#1F3E6D",
         "line-M5": "#9778D3",
       },
     },
