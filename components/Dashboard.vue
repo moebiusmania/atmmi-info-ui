6eebe26fa4518a42117af2a60a9618df55f27dfd
moebiusmania <moebiusmania@gmail.com>
2023-03-23 21:31:35 +0100

    fix: add container class to main wrappers

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="px-4 py-2 grid gap-5 md:grid-cols-2 lg:grid-cols-3">
+  <main class="container mx-auto px-4 py-2 grid gap-5 md:grid-cols-2 lg:grid-cols-3">
     <Status />
     <Traffic />
     <News />
--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="navbar mb-2 shadow-lg bg-primary text-white">
-    <div class="flex-1 px-2 mx-2">
-      <h1 class="text-lg font-bold">ATM Dashboard</h1>
+    <div class="flex-1 px-2 mx-auto container">
+      <h1 class="text-lg font-bold mx-2">ATM Dashboard</h1>
     </div>
   </header>
 </template>
