b316596d316907c0ed5ca679b83f2e955f3a0089
moebiusmania <moebiusmania@gmail.com>
2023-03-23 21:32:39 +0100

    feat: add default value during lazy loading (optimistic update)

--- components/News.vue
+++ components/News.vue
@@ -1,29 +1,21 @@
 <script lang="ts" setup>
-import { loadingClass } from "./utils";
+import { loadingClass, mockNews } from "./utils";
 import type { News } from "../types/news";
 
 const resource: string = "/api/news";
 
-const { pending, data } = useLazyFetch<Array<News>>(resource)
+const { pending, data } = useLazyFetch<Array<News>>(resource, {
+  default: mockNews
+})
 </script>
 
 <template>
   <Card title="ATM informa">
     <ul>
-      <li
-        v-for="(item, index) in data"
-        :key="index"
-        :class="pending ? 'my-2 animate-pulse' : 'list-disc list-inside'"
-      >
+      <li v-for="(item, index) in data" :key="index" :class="pending ? 'my-2 animate-pulse' : 'list-disc list-inside'">
         <div v-if="pending" :class="loadingClass()"></div>
-        <a
-          v-else
-          :href="item.url"
-          target="_blank"
-          rel="nofollow noopener"
-          class="hover:underline hover:text-primary"
-          >{{ item.text }}</a
-        >
+        <a v-else :href="item.url" target="_blank" rel="nofollow noopener" class="hover:underline hover:text-primary">{{
+          item.text }}</a>
       </li>
     </ul>
   </Card>
--- components/Status.vue
+++ components/Status.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { loadingClass } from "./utils";
+import { loadingClass, mockLine } from "./utils";
 import type { LineStatus } from "../types/line";
 
 const resource: string = "/api/status";
@@ -7,24 +7,20 @@ const resource: string = "/api/status";
 const lineClass = (line: string): string =>
   `badge bg-line-${line} rounded-none border-0`;
 
-const { pending, data } = useLazyFetch<Array<LineStatus>>(resource)
+const { pending, data } = useLazyFetch<Array<LineStatus>>(resource, {
+  default: mockLine
+})
 </script>
 
 <template>
   <Card title="Status linee MM">
     <ul>
-      <li
-        v-for="(item, index) in data"
-        :key="index"
-        :class="pending ? 'my-2 animate-pulse' : ''"
-      >
+      <li v-for="(item, index) in data" :key="index" :class="pending ? 'my-2 animate-pulse' : ''">
         <div :class="pending ? loadingClass() : lineClass(item.line)">
           <span v-if="!pending">{{ item.line }}</span>
         </div>
         {{ " " }}
-        <span v-if="!pending"
-          >{{ item.text }} |{{ " " }} {{ item.status }}{{ " " }}</span
-        >
+        <span v-if="!pending">{{ item.text }} |{{ " " }} {{ item.status }}{{ " " }}</span>
       </li>
     </ul>
   </Card>
