c3c1d1d46efde47dfed60097ecfcf52e3fa46adc
moebiusmania <moebiusmania@gmail.com>
2023-06-19 21:45:08 +0200

    fix: using vanilla fetch on server requests

--- server/endpoint.ts
+++ server/endpoint.ts
@@ -1,9 +1,12 @@
 const ENDPOINT: string = "https://www.atm.it/it/Pagine/default.aspx";
 
-const getPage = async (): Promise<string> =>
-  $fetch(ENDPOINT, {
+const getPage = async (): Promise<string> => {
+  const response = await fetch(ENDPOINT, {
     method: "GET",
     mode: "cors",
   });
+
+  return response.text();
+};
 
 export { ENDPOINT, getPage };
