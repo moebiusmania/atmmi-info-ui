49cffe6e55e2dbb53e31e7919f760245594d7eb2
moebiusmania <moebiusmania@gmail.com>
2024-02-10 00:24:55 +0100

    test: fixing workflow

---
+++ components/__tests__/Card.test.ts
@@ -0,0 +1,22 @@
+import { mount } from "@vue/test-utils";
+import { expect, test } from "vitest";
+
+import Card from "../Card.server.vue";
+
+test("<Card />", () => {
+  const title = "Test title";
+  const content = "Test content";
+
+  const wrapper = mount(Card, {
+    props: {
+      title,
+    },
+    slots: {
+      default: `<p>${content}</p>`,
+    },
+  });
+
+  expect(wrapper.find("article").exists()).toBe(true);
+  expect(wrapper.text()).toContain(title);
+  expect(wrapper.text()).toContain(content);
+});
