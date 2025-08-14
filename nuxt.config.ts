fbeae5b088e7136c339550c495d36e0848619401
Salvatore Laisa <salvatore.laisa@carol.health>
2025-08-14 11:08:17 +0200

    feat: upgrading to nuxt 4 and daisyui 5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: ["nuxt-icon"],
 
   experimental: {
     componentIslands: true,
   },
 
   compatibilityDate: "2024-10-26",
+
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
